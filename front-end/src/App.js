95a9753d2834ec0e836cfc345b5e2f44d429388c
taiz <llamttai@gmail.com>
2024-02-20 10:45:03 +0700

    add folder

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "react-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header></Header>
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+            <Footer></Footer>
+        </div>
+    );
 }
 
 export default App;
