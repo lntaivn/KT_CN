d16057e8d95b0f54c52781bca4e24a57e52f92c6
kakanvk <caoka5265@gmail.com>
2024-02-20 08:08:49 +0700

    change project

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-Route::get('/hello', function () {
-    return view('hello');
+Route::get('/home', function () {
+    return view('welcome');
 });
