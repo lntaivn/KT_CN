502cba9ffbb9d16b1642ea3829b8b0f2ef7cb0cd
taiz <llamttai@gmail.com>
2024-02-27 15:58:55 +0700

    4:00

--- back-end/database/migrations/2024_02_24_130932_create_category.php
+++ back-end/database/migrations/2024_02_24_130932_create_category.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -15,7 +14,8 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id('id_category'); // Tạo cột id_category là primary key
-            $table->string('name'); // Tạo cột name là string
+            $table->string('name_vi', 50);
+            $table->string('name_vi', 50);
             $table->timestamps(); // Thêm cột created_at và updated_at
         });
     }
