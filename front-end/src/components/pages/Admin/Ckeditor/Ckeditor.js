298b16c33c7dee1e80895ec46c3b69f177106018
thaihunghung <110120031@st.tvu.edu.vn>
2024-02-28 16:07:40 +0700

    delete img

--- front-end/src/components/pages/Admin/Ckeditor/Ckeditor.js
+++ front-end/src/components/pages/Admin/Ckeditor/Ckeditor.js
@@ -1,6 +1,24 @@
 import React, { useState } from 'react';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { Collapse } from 'antd';
+
+const { Panel } = Collapse;
+
+const items = [
+  {
+    key: '1',
+    label: 'Tạo bài viết tiếng việt',
+    children: "<p>Hello from CKEditor 5!</p>",
+  },
+  {
+    key: '2',
+    label: 'Tạo bài viết tiếng anh',
+    children: "<p>Hello from CKEditor 5!</p>",
+  }
+];
+
+
 
 const Ckeditor  = ({ initialValue }) => {
     const [editorData, setEditorData] = useState(initialValue);
@@ -12,24 +30,33 @@ const Ckeditor  = ({ initialValue }) => {
 
     return (
         <div>
-            <h2>Using CKEditor 5 build in React</h2>
-            <CKEditor
-                editor={ ClassicEditor }
-                data="<p>Hello from CKEditor 5!</p>"
-                onReady={ editor => {
-                    console.log( 'Editor is ready to use!', editor );
-                } }
-                onChange={ ( event, editor ) => {
-                    console.log( 'Data:', editor.getData() );
-                } }
-                onBlur={ ( event, editor ) => {
-                    console.log( 'Blur.', editor );
-                } }
-                onFocus={ ( event, editor ) => {
-                    console.log( 'Focus.', editor );
-                } }
-                config={ { ckfinder: { uploadUrl: 'http://127.0.0.1:8000/api/admin/upload-image' } } }
-            />
+            <form>
+                đơn
+            </form>
+             <Collapse>
+                {items.map(item => (
+                    <Panel key={item.key} header={item.label}>
+                        <CKEditor
+                            editor={ClassicEditor}
+                            data={item.children}
+                            onChange={(event, editor) => {
+                                console.log('Data:', editor.getData());
+                            }}
+                            onBlur={(event, editor) => {
+                                console.log('Blur.', editor);
+                            }}
+                            onFocus={(event, editor) => {
+                                console.log('Focus.', editor);
+                            }}
+                            config={{
+                                ckfinder: {
+                                    uploadUrl: 'http://127.0.0.1:8000/api/admin/upload-image'
+                                }
+                            }}
+                        />
+                    </Panel>
+                ))}
+            </Collapse>
         </div>
     );
 }
