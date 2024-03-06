52a4af40121cab9e3cf45a4eee55017f3f454ca5
thaihunghung <110120031@st.tvu.edu.vn>
2024-03-06 09:05:51 +0700

    delete-softway

--- back-end/app/Models/News.php
+++ back-end/app/Models/News.php
@@ -20,8 +20,9 @@ class News extends Model
         'status_vi',
         'status_en',
         'thumbnail',
+        'is_deleted',
         'view_count',
-        'update_by',
+        'update_by'
     ];
 
     // Xác định mối quan hệ với các model khác
