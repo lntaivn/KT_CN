6b63f23499736b2a567bffa2dcc05dbfa4826792
thaihunghung <110120031@st.tvu.edu.vn>
2024-03-18 16:01:46 +0700

    7hhh

--- front-end/src/components/pages/Client/About/About.js
+++ front-end/src/components/pages/Client/About/About.js
@@ -5,10 +5,10 @@ const About = () => {
     const { t } = useTranslation();
 
     return ( 
-        <div className='flex w-full	 items-center justify-center'>
-            <div className='w-[1140px] p-5'>
-                <h1> {t('about.text_about_1')}</h1>
-                <h2>{t('about.text_about_2')}</h2>
+        <div className='flex w-full	 items-center justify-center text-justify leading-7'>
+            <div className='w-[1140px] p-5 flex flex-col gap-[10px]'>
+                <h1 className='p-5 font-bold text-center text-2xl'> {t('about.text_about_1')}</h1>
+                <h2 className='text-center text-xl'>{t('about.text_about_2')}</h2>
                 <p>
                     {t('about.text_about_body_1')}
                 </p>
